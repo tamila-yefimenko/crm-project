b131c78961163f6aa1f74e1494573ee701af7cd5
Tamila Yefimenko <tamilaedu88@gmail.com>
2025-09-21 13:50:51 +0300

    Added modal for adding company, fixed prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
-import StatusLabel, {Status} from "@/app/components/status-label";
-import React from "react";
+import { headers } from 'next/headers';
+import AddCompanyButton from '@/app/components/add-company-button';
 
 export default function Home() {
-    return (
-        <main>
-            <h1 className="text-xl">Home page</h1>
-            <StatusLabel status={Status.Active}>Active</StatusLabel>
-            <StatusLabel status={Status.NotActive}>Not active</StatusLabel>
-            <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-            <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
-        </main>
-    );
+  headers();
+  return (
+    <main>
+      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
+      <AddCompanyButton />
+    </main>
+  );
 }
